1758169a45967394f659a67786a1a6f3dee9126e
lotan <lotanro@gmail.com>
2020-05-10 09:08:21 +0700

    first

---
+++ review/screens/89912a05-afc3-41d4-84b2-2956d34f725f.js
@@ -0,0 +1,61 @@
+var content='<div class="ui-page" deviceName="web" deviceType="desktop" deviceWidth="1024" deviceHeight="768">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 1" width="1024" height="768">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1589075344964.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1589075344964-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1589075344964-ie8.css" /><![endif]-->\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-89912a05-afc3-41d4-84b2-2956d34f725f" class="screen growth-both devWeb canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="51" width="1024" height="768">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/89912a05-afc3-41d4-84b2-2956d34f725f-1589075344964.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/screens/89912a05-afc3-41d4-84b2-2956d34f725f-1589075344964-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/screens/89912a05-afc3-41d4-84b2-2956d34f725f-1589075344964-ie8.css" /><![endif]-->\
+      <div id="s-Image_1" class="pie image firer ie-background commentable non-processed"   datasizewidth="1030px" datasizeheight="583px" dataX="0" dataY="0"   alt="image">\
+          <img src="./images/2d0c2f84-99ec-4aff-81dc-3aa60f58c219.jpg" />\
+      </div>\
+\
+      <div id="s-Image_2" class="pie image firer ie-background commentable non-processed"   datasizewidth="1033px" datasizeheight="526px" dataX="3" dataY="572"   alt="image">\
+          <img src="./images/93a9fc7a-fe2b-400f-ac94-637032fbb56b.jpg" />\
+      </div>\
+\
+      <div id="s-Image_3" class="pie image firer ie-background commentable non-processed"   datasizewidth="1038px" datasizeheight="580px" dataX="0" dataY="1098"   alt="image">\
+          <img src="./images/e39946c9-075d-4659-8c23-a88ef6814a1b.jpg" />\
+      </div>\
+      <div id="s-Hotspot_1" class="imagemap firer click ie-background commentable non-processed"   datasizewidth="440px" datasizeheight="40px" dataX="417" dataY="10"  >\
+          <div class="clickableSpot"></div>\
+      </div>\
+      <div id="s-Hotspot_5" class="imagemap firer click ie-background commentable non-processed"   datasizewidth="123px" datasizeheight="32px" dataX="893" dataY="12"  >\
+          <div class="clickableSpot"></div>\
+      </div>\
+      <div id="shapewrapper-s-Line_1" class="shapewrapper shapewrapper-s-Line_1 non-processed"  rotationdeg="90" datasizewidth="429px" datasizeheight="11px" dataX="747" dataY="299" >\
+          <svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" id="svg-s-Line_1" class="svgContainer" style="width:100%;height:100%;">\
+              <g>\
+                  <g>\
+                      <path id="s-Line_1" class="pie line shape non-processed-shape firer ie-background commentable non-processed" d="M 0 5 L 429 5"  marker-end="url(#end-marker-s-Line_1">\
+                      </path>\
+                  </g>\
+              </g>\
+              <defs>\
+      			<marker id="end-marker-s-Line_1" class="open endmarker" orient="0" stroke-width="0px" viewBox="0 0 100 100" refY="50" preserveAspectRatio="none" markerUnits="userSpaceOnUse">\
+      				<path d="M 39.688716 40.466926 39.818418 60.051881 7.9118028 79.24773 C -6.6058565 88.636494 5.3977603 106.07944 19.844358 97.146562 L 99.610893 50.324254 21.53048 3.7613489 C 4.631474 -8.1505951 -6.7257532 14.353316 7.6523994 20.881971 Z"></path>\
+      			</marker>\
+              </defs>\
+          </svg>\
+      </div>\
+      <div id="s-Text_1" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="60px" datasizeheight="22px" dataX="914" dataY="19" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_1_0">משתמש</span></div></div></div></div>\
+      <div id="s-Text_10" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="152px" datasizeheight="42px" dataX="28" dataY="9" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_10_0">ReaLearn</span></div></div></div></div>\
+      <div id="s-Text_11" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="130px" datasizeheight="22px" dataX="45" dataY="79" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_11_0">קישור לעוד חומרים</span></div></div></div></div>\
+      <div id="s-Hotspot_2" class="imagemap firer click ie-background commentable non-processed"   datasizewidth="145px" datasizeheight="28px" dataX="28" dataY="76"  >\
+          <div class="clickableSpot"></div>\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
---
+++ review/screens/f76382bb-5b34-4b25-a5cc-84419b21f470.js
@@ -0,0 +1,55 @@
+var content='<div class="ui-page" deviceName="web" deviceType="desktop" deviceWidth="1024" deviceHeight="768">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 1" width="1024" height="768">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1589075344964.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1589075344964-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1589075344964-ie8.css" /><![endif]-->\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-f76382bb-5b34-4b25-a5cc-84419b21f470" class="screen growth-both devWeb canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="3" width="1024" height="768">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/f76382bb-5b34-4b25-a5cc-84419b21f470-1589075344964.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/screens/f76382bb-5b34-4b25-a5cc-84419b21f470-1589075344964-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/screens/f76382bb-5b34-4b25-a5cc-84419b21f470-1589075344964-ie8.css" /><![endif]-->\
+      <div id="s-Image_1" class="pie image firer ie-background commentable non-processed"   datasizewidth="1024px" datasizeheight="600px" dataX="0" dataY="0"   alt="image">\
+          <img src="./images/245b0f6f-6076-418b-aa29-e5e14e86d851.jpg" />\
+      </div>\
+      <div id="s-Rectangle_1" class="pie rectangle firer commentable non-processed"   datasizewidth="713px" datasizeheight="46px" dataX="145" dataY="0" >\
+       <div class="backgroundLayer"></div>\
+       <div class="paddingLayer">\
+         <div class="clipping">\
+           <div class="content">\
+             <div class="valign">\
+               <span id="rtr-s-Rectangle_1_0"></span>\
+             </div>\
+           </div>\
+         </div>\
+       </div>\
+      </div>\
+      <div id="s-Text_2" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="94px" datasizeheight="22px" dataX="762" dataY="28" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_2_0">האתגרים שלי</span></div></div></div></div>\
+      <div id="s-Text_3" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="139px" datasizeheight="22px" dataX="591" dataY="28" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_3_0">מערכת הלמידה שלי</span></div></div></div></div>\
+      <div id="s-Text_4" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="90px" datasizeheight="22px" dataX="457" dataY="28" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_4_0">הקבוצות שלי</span></div></div></div></div>\
+      <div id="s-Text_5" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="170px" datasizeheight="32px" dataX="426" dataY="143" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_5_0">תיק העבודות שלי</span></div></div></div></div>\
+      <div id="s-Text_6" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="98px" datasizeheight="22px" dataX="390" dataY="433" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_6_0">תגים שרכשתי</span></div></div></div></div>\
+      <div id="s-Hotspot_1" class="imagemap firer click ie-background commentable non-processed"   datasizewidth="440px" datasizeheight="40px" dataX="417" dataY="10"  >\
+          <div class="clickableSpot"></div>\
+      </div>\
+      <div id="s-Hotspot_5" class="imagemap firer click ie-background commentable non-processed"   datasizewidth="123px" datasizeheight="32px" dataX="882" dataY="14"  >\
+          <div class="clickableSpot"></div>\
+      </div>\
+      <div id="s-Text_1" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="60px" datasizeheight="22px" dataX="914" dataY="19" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_1_0">משתמש</span></div></div></div></div>\
+      <div id="s-Text_10" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="152px" datasizeheight="42px" dataX="31" dataY="2" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_10_0">ReaLearn</span></div></div></div></div>\
+      <div id="s-Text_11" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="130px" datasizeheight="22px" dataX="45" dataY="79" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_11_0">קישור לעוד חומרים</span></div></div></div></div>\
+      <div id="s-Hotspot_2" class="imagemap firer click ie-background commentable non-processed"   datasizewidth="145px" datasizeheight="28px" dataX="22" dataY="76"  >\
+          <div class="clickableSpot"></div>\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
